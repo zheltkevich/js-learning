5a6c20ad13c640264e2a3d2b5d412caa9391e300
Aleksandr <zheltkevich42@gmail.com>
2021-01-15 01:31:49 +0300

    add lesson 01

---
+++ js/1.js
@@ -0,0 +1,49 @@
+alert('First JS connection type');
+
+//однострочный коментарий
+
+/*
+многострочный
+коментарий
+*/
+
+alert('literal - так называют значение в круглых скобках функции'); // литерал или значение
+
+// 'literal' - строчный литерал
+// 12345 - числовой литерал
+// true / false - логический / булевый литерал
+// null - пустой литерал
+// undefined - неопределенный литерал
+
+// 
+
+let myVariant = 'myVariant' // переменная
+
+alert(myVariant);
+
+// 
+
+let noname
+
+noname = 'pupok'
+
+alert(noname);
+
+// 
+
+let a = 1000000
+let b = 10
+let c = 20
+
+a = b + c
+
+alert(a);
+
+//
+
+let number = 50
+let string = '50'
+alert(number + string);
+alert(number - string);
+alert(number * string);
+alert(number / string);
