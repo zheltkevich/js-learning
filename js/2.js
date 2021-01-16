9e26506bef3047b25b49370b3e4a3211f5d8d187
Aleksandr <zheltkevich42@gmail.com>
2021-01-16 23:35:33 +0300

    add lesson 2

---
+++ js/2.js
@@ -0,0 +1,21 @@
+// let a = 0.3 - 0.2; /// 0.0999999
+// let b = 0.2 - 0.1; /// 0.1
+
+// alert(b);
+// alert(a);
+
+// a = 10;
+// b = a++;
+// alert(a);
+// alert(b);
+
+// let a = 10;
+// let b = ++a;
+// alert(b);
+
+let c = 10;
+let d = 5;
+
+c += d;
+
+alert(c);
